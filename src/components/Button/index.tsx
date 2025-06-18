5490242f2e07750838fad2f9574e750165eeedac
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-18 18:48:37 +0200

    added substraction

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -8,7 +8,7 @@ export default function Button({ children, onClick}: ButtonProps) {
     <>
       <button
         onClick={onClick}
-        className=" transition-all cursor-pointer active:bg-blue-50 bg-blue-800 w-10 h-10 rounded-2xl font-black hover:bg-blue-500 "
+        className=" m-1 transition-all cursor-pointer active:bg-blue-50 bg-blue-800 w-10 h-10 rounded-2xl font-black hover:bg-blue-500 "
       >
         {children}
       </button>
--- src/components/CenterContainer/index.tsx
+++ src/components/CenterContainer/index.tsx
@@ -1,16 +1,21 @@
 "use client";
-import { useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import Button from "../Button";
 export default function CenterContainer() {
-  const [count, setCount] = useState(0);
-
-  const handlePlus = () => setCount(count + 1);
+  
+  // State Logic
+  const [count, setCount]: [number, Dispatch<SetStateAction<number>>] = useState(0);
+  const handlePlus: () => void = () => setCount(count + 1);
+  const handleMinus: () => void = () => setCount(count - 1);
 
   return (
     <>
       <section className="flex flex-col justify-center items-center content-center rounded-2xl shadow-2xl shadow-amber-500  fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-amber-50 w-50 h-50">
         <h2 className="text-black text-9xl">{count}</h2>
-        <Button onClick={handlePlus}>+</Button>
+        <div>
+          <Button onClick={handlePlus}>+</Button>
+          <Button onClick={handleMinus}>-</Button>
+        </div>
       </section>
     </>
   );
