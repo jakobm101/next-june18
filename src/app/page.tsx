9c06c057aa9014e69c704c740d93bcd39f0e480f
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-18 18:09:30 +0200

    count prep

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import CenterGridLines from "@/components/CenterGridLines";
+import CenterContainer from "@/components/CenterContainer";
 
 export default function Home() {
   return (
     <>
-      <section className=" rounded-2xl shadow-2xl shadow-amber-500  fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-amber-50 w-50 h-50"></section>
-      <CenterGridLines/>
+      <CenterContainer />
+      <CenterGridLines />
     </>
   );
 }
---
+++ src/components/CenterContainer/index.tsx
@@ -0,0 +1,14 @@
+"use client"
+import { useState } from "react";
+export default function CenterContainer() {
+const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section className="flex flex-col justify-center items-center content-center rounded-2xl shadow-2xl shadow-amber-500  fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-amber-50 w-50 h-50">
+      <h2 className="text-black text-9xl">{count}</h2>
+      <button className="bg-blue-800 w-10 h-10 rounded-2xl font-black hover:bg-blue-500 ">+</button>
+      </section>
+    </>
+  );
+}
