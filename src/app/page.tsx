1dc2402df45f6a595a5905af97d2eebe4f6901df
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-18 17:56:56 +0200

    compnent training

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
+import CenterGridLines from "@/components/CenterGridLines";
 
 export default function Home() {
   return (
     <>
       <section className=" rounded-2xl shadow-2xl shadow-amber-500  fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-amber-50 w-50 h-50"></section>
-      <div className="fixed bg-amber-500 h-full w-px left-1/2"></div>
-      <div className="fixed bg-amber-500 h-px top-1/2 left-0 w-full"></div>
+      <CenterGridLines/>
     </>
   );
 }
---
+++ src/components/CenterGridLines/index.tsx
@@ -0,0 +1,8 @@
+export default function CenterGridLines() {
+  return (
+    <>
+      <div className="fixed bg-amber-500 h-full w-px left-1/2"></div>
+      <div className="fixed bg-amber-500 h-px top-1/2 left-0 w-full"></div>
+    </>
+  );
+}
